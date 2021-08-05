7ea42a844545a56338a7013725bb390bd95c5452
appletri <tri_duc_lam@hotmail.com>
2021-08-04 21:56:38 -0600

    added random stars and new planet picture

--- scripts.js
+++ scripts.js
@@ -118,7 +118,7 @@ resetButton.addEventListener(`click`, function() {
 function playGame(){
   equationBox.removeEventListener(`mouseover`, playGame);
   equationBox.addEventListener("mouseout", alertRed);
-  gameTime = 15; //debugging
+  gameTime = 60; //debugging
   constantGameTime = gameTime;
   gameState = "playgame";
   assignColors();
@@ -566,3 +566,77 @@ function updateArray() {
 function difficulty() {
     
 }
+
+function Star(x,y,r,color){
+    this.x = x;
+    this.y = y;
+    this.r = r;
+    this.rChange = 0.015;
+    // this.vx = Math.floor(Math.random()*4+1);
+    // this.vy = Math.floor(Math.random()*4+1);
+    this.color = color;
+}
+
+Star.prototype = {
+    constructor: Star,
+    render: function(){
+      context.beginPath();
+      context.arc(this.x, this.y, this.r, 0, 2*Math.PI, false);
+      context.shadowBlur = 8; 
+      context.shadowColor = "white";
+      context.fillStyle = this.color;
+      context.fill();
+    },
+    update: function(){
+      
+       if (this.r > 2 || this.r < .8){
+           this.rChange = - this.rChange;
+       }
+       this.r += this.rChange;
+    }
+}
+
+
+// Star functionality
+
+let canvas = document.getElementById("canvas");
+let context = canvas.getContext("2d");
+
+let C_WIDTH = canvas.width = document.body.offsetWidth;
+let C_HEIGHT = canvas.height = document.body.offsetHeight;
+
+function randomColor(){
+        let arrColors = ["ffffff", "ffecd3" , "bfcfff"];
+        return "#"+arrColors[Math.floor((Math.random()*3))];
+}
+        
+var arrStars = [];
+for(i = 0; i < 100; i++){
+    let randX = Math.floor((Math.random()*C_WIDTH)+1);
+    let randY = Math.floor((Math.random()*C_HEIGHT)+1);
+    let randR = Math.random() * 1.7 + .5;
+    
+    let star = new Star(randX, randY, randR, randomColor());
+    arrStars.push(star);
+}
+function update(){
+  for(i = 0; i < arrStars.length; i ++){
+    arrStars[i].update();
+  }
+}
+function animate(){
+  update();
+  /*
+    Remove comments below these for a cool trailing effect & comment
+    out the context.clearRect.
+  */
+    //context.fillStyle = 'rgba(255, 255, 255, .1)';
+    //context.fillRect(0,0,C_WIDTH,C_HEIGHT);
+    context.clearRect(0,0,C_WIDTH,C_HEIGHT);
+    for(var i = 0; i < arrStars.length; i++){
+      arrStars[i].render();
+    }
+    requestAnimationFrame(animate);
+}
+
+animate();
